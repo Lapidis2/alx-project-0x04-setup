a58166d0dbc08e60cb191b4b13b2f23e575664e8
Lapidis2 <hitlapidis2@gmail.com>
2025-07-29 12:55:59 +0200

    creating store file

---
+++ alx-project-0x06/pages/counter-app.tsx
@@ -0,0 +1,26 @@
+import { useCount } from "@/context/CountContext";
+
+const CounterApp = () => {
+  const { count, increment, decrement } = useCount();
+
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-3xl font-bold mb-4">Counter App</h1>
+      <p className="text-xl mb-2">Current Count: {count}</p>
+      <button
+        className="bg-green-500 text-white px-4 py-2 mr-2"
+        onClick={increment}
+      >
+        Increment
+      </button>
+      <button
+        className="bg-red-500 text-white px-4 py-2"
+        onClick={decrement}
+      >
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default CounterApp;
