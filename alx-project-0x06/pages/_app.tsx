e8d34cc194ae0948cdea4099866270b79b627328
Lapidis2 <hitlapidis2@gmail.com>
2025-07-29 13:08:59 +0200

    updates _app.tsx file

--- alx-project-0x06/pages/_app.tsx
+++ alx-project-0x06/pages/_app.tsx
@@ -1,14 +1,16 @@
-import Layout from "@/components/layouts/Layout";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { CountProvider } from "@/context/CountContext";
+
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '@/components/layouts/Layout';
+import { Provider } from 'react-redux';
+import store from '@/store/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CountProvider>
+    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </CountProvider>
-  )
+    </Provider>
+  );
 }
