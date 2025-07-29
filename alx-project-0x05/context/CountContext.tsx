744bca50fdf4b4033fe5c8762c8c82bf7b0d45e1
Lapidis2 <hitlapidis2@gmail.com>
2025-07-29 10:55:22 +0200

    solving error for type checking

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,11 +1,11 @@
 import {createContext,useContext,ReactNode, useState} from "react"
 
-interface CountContextProps {
-	count:number,
-	increment: ()=>void,
-	decrement: ()=>void
-}
 
+interface CountContextProps {
+	count: number
+	increment: () => void
+	decrement: () => void
+  }
 export const CountContext =createContext<CountContextProps|undefined>(undefined)
 export const CountProvider =({children}:{children:ReactNode})=>{
 
