c4b01cdc85c560280c844a2158af4ed207979731
Lapidis2 <hitlapidis2@gmail.com>
2025-08-04 21:49:31 +0200

    changing favicon and title

--- alx-project-0x05/pages/_document.tsx
+++ alx-project-0x05/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+		 <Head>
+        <title>My Next.js App</title>
+        <meta name="description" content="This is a great app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    
       <body className="antialiased">
         <Main />
         <NextScript />
